34db549b8624a9428705af30019eeaa09d57d76c
Abhijit-Dixit <abhijitdixitent@gmail.com>
2021-03-16 16:32:45 +0530

    Used jpa for storing conversion rates

--- currency-exchange-service/src/main/java/com/abhijit/microservices/currencyexchangeservice/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/abhijit/microservices/currencyexchangeservice/ExchangeValue.java
@@ -1,14 +1,23 @@
 package com.abhijit.microservices.currencyexchangeservice;
 
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class ExchangeValue {
+	@Id
 	private long id;
+	
+	@Column(name="currency_from")
 	private String from;
+	@Column(name="currency_to")
 	private String to;
 	private double conversionMultiple;
 	private int port;
